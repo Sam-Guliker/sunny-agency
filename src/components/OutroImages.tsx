eeb9b904d38aa09ed04a9bb8c38af5439d47f04f
Sam Guliker <samguliker@Sams-MacBook-Air.local>
2022-01-01 10:38:08 +0100

    section update

--- src/components/OutroImages.tsx
+++ src/components/OutroImages.tsx
@@ -5,11 +5,11 @@ import cubes from '../assets/mobile/image-gallery-sugar-cubes.jpg'
 
 export function OutroImages(){
     return (    
-        <section data-scroll-section className="outro-images-container">
+        <div className="outro-images-container">
             <img data-scroll className="image" src={milk} alt={milk} />
             <img data-scroll className="image" src={orangeTwo} alt={orangeTwo} />
             <img data-scroll className="image" src={cone} alt={cone} />
             <img data-scroll className="image" src={cubes} alt={cubes} />
-        </section>
+        </div>
     )
 }
--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -7,13 +7,13 @@ type TestimonialCardProps = {
 
 export function TestimonialCard ({img, desc, name, jobTitle}: TestimonialCardProps) {
     return (
-    <section className="testimonial-card">
+    <div className="testimonial-card">
         <article className="container">
             <img className='testimonial-card-img' src={img} alt={img} />
             <p className="paragraph-01 paragraph-01--extra-p-30">{desc}</p>
             <h3 className="heading-03">{name}</h3>
             <p className="paragraph-02">{jobTitle}</p>
         </article>
-    </section>
+    </div>
     )
 }
